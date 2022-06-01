40d1795eaa6ca463c5f657b65748f15344ad6639
Kevin Amador <k.amador5@icloud.com>
2022-06-01 00:12:21 -0600

    Initial Commit

---
+++ ZemogaTest/ZemogaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZemogaTestApp.swift
+//  ZemogaTest
+//
+//  Created by Kevin Amador Rios on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ZemogaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
